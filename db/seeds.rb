095ae0066f0f3fabbf921007b65b5294a7cea779
ajithkgatty <=>
2018-09-06 21:36:56 +0530

    documents scaffold, seed data and rack-cors gem enabling

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#create some dummy entries for freelance documents
+10.times do |i|
+    FreelanceDocument.create!(
+      title: "document #{i+1}",
+      description: "The spacecraft’s sole mission (which it had no choice but to accept as 
+                it was an uncrewed robotic spacecraft and not a sentient being) was to survey
+                 the surface of the moon. NASA, the federal government, and the American publi0
+                 c were all eager to see the promise of slain President John F. Kennedy fulfilled: By the 
+                end of the decade, the United States would put a man on the moon.",
+      file_url: "www.google.com",
+      image_url: "www.google.com"
+    )
+  end
